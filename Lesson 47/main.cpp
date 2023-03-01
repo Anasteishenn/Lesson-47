e2fbbddb077573cfdac29972e5b372b3d61744b9
Bazylevich <victoria.bazylevich@gmail.com>
2023-03-01 18:13:12 +0300

    add funcsion

--- Lesson 47/main.cpp
+++ Lesson 47/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 void rand_init(int* vector, int size, int bound);
+int max(int* vector, int size);
+int min(int* vector, int size);
+string convert(int* vector, int size);
 int main() {
 	int size;
 	cout << "Input size of array: " << endl;
